261210d8a7a251777c132ccc0930831fbed46831
蔡申傲 <3094165094@qq.com>
2021-11-25 22:01:11 +0800

    为controller(控制层)的StudentController添加TODO

---
+++ controller/AdminController.py
@@ -0,0 +1,13 @@
+"""
+    管理员的控制层
+"""
+from DAO import AdminMapper
+
+def login(username, password):
+    """
+    TODO 管理员登录的控制层 (要做简单的校验--校验是否为空)
+    :param username: 账号
+    :param password: 密码
+    :return:
+    """
+    pass
