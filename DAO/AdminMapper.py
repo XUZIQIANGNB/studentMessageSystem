efe7415130b83f5a4a31ef4d85779c9bf7b8b78b
蔡申傲 <3094165094@qq.com>
2021-11-26 18:06:50 +0800

    DAO的基础业务实现

--- DAO/AdminMapper.py
+++ DAO/AdminMapper.py
@@ -5,17 +5,37 @@ import sqlite3
 
 def login(username, password):
     """
-    TODO sql语句查询
+     sql语句查询
     :param username: 账号
     :param password: 密码
     :return: Ture|False
     """
-    connection = sqlite3.connect('sqlite_system.db')
-    connection
+    # 连接数据库
+    connection = sqlite3.connect(r'../sqlite/student_system.db')
+    # 获取一个游标
+    cursor = connection.cursor()
+    # 定义sql语句
+    sql = """select * from admin where username = ? and password = ?"""
+    # 执行sql语句
+    cursor.execute(sql, (username, password))
+    # 获取游标执行sql语句的结果集
+    result = cursor.fetchall()
+    # 关闭游标
+    cursor.close()
+    # 关闭连接
     connection.close()
 
+    if len(result) == 0:
+        return False
+    else:
+        return True
 
-
+"""
+    此模块的测试
+"""
 if __name__ == '__main__':
-
-    login('王坚', '147258')
+    is_login = login('王坚', '14725')
+    if is_login:
+        print('登录成功')
+    else:
+        print('登录失败')
--- DAO/StudentMapper.py
+++ DAO/StudentMapper.py
@@ -3,52 +3,170 @@
 """
 
 import sqlite3
-
+from entry.Student import Student
 def add_student(student):
     """
-    TODO 向student表中添加一名学生信息
+    向student表中添加一名学生信息
     :param student: student对象
-    :return: None
+    :return: 受影响数
     """
-    pass
+    # 插入学生的id必须为空
+    if student.id is not None:
+        return 0
+    connection = sqlite3.connect(r'../sqlite/student_system.db')
+    sql = """insert into student(name ,age, gender, cls) values (?,?,?,?)"""
+    cursor = connection.cursor()
+    cursor.execute(sql, (student.name, student.age, student.gender, student.cls))
+    cursor.close()
+    # 注意：sqlite 添加是默认开启事务的，所以需要提交事务
+    connection.commit()
+    connection.close()
+    # 将受影响的函数 返回
+    return cursor.rowcount
+
 def delete_student(id):
     """
-    TODO 根据学生的学号，删除表中对象的学生信息
+    根据学生的学号，删除表中对象的学生信息
     :param id: 学号
-    :return: None
+    :return: 受影响的行数，0为操作失败
     """
-    pass
+    # 简单的数据校验
+    if isinstance(id, int) is not True and id <= 0:
+        return 0
+    connection = sqlite3.connect(r'../sqlite/student_system.db')
+    cursor = connection.cursor()
+    sql = """delete from student where id = ?"""
+    cursor.execute(sql, (id,))
+    cursor.close()
+    # 删除默认也是开启事务的，需要进行提交
+    connection.commit()
+    connection.close()
+    return cursor.rowcount
 
 def select_one_student(id):
     """
-    TODO 根据学生的id查找表中对应的信息
+    根据学生的id查找表中对应的信息
     :param id: 学号
     :return: Student
     """
-    pass
-
+    # 简单校验
+    if isinstance(id, int) is not True and id <= 0:
+        return
+    connection = sqlite3.connect(r'../sqlite/student_system.db')
+    cursor = connection.cursor()
+    sql = "select id, name, age, gender, cls from student where id = ?"
+    cursor.execute(sql, (id, ))
+    # 获取结果集
+    result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    # print(result[0])
+    # 判断结果
+    if result is None:
+        return None
+    else:
+        id, name, age, gender, cls = result[0]
+        student = Student(id=id, name=name, age=age, gender=gender, cls=cls)
+        print(student.getmessage())
 def select_all_student():
     """
-    TODO 查询student表中的所有信息，并将其封装为列表
+    查询student表中的所有信息，并将其封装为列表
     :return:list(Student)
     """
-    pass
+    connection = sqlite3.connect(r'../sqlite/student_system.db')
+    cursor = connection.cursor()
+    sql = "select id, name, age, gender, cls from student"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    # 关闭连接
+    cursor.close()
+    connection.close()
+    # 封装查询数据
+    if result is None:
+        return None
+    student_list = []
+    for student_tuple in result:
+        id, name, age, gender, cls = student_tuple
+        student = Student(id=id, name=name, age=age, gender=gender, cls=cls)
+        student_list.append(student)
+    return student_list
 
 def select_special_student(sql_special):
     """
-    TODO 特殊sql语句的查询 并将其结果封装为Student类型的列表
+    特殊sql语句的查询 并将其结果封装为Student类型的列表
     :param sql_special: 特殊的sql条件
     :return: list(Student)
     """
-    pass
+    if sql_special is None:
+        return None
+    connection = sqlite3.connect(r'../sqlite/student_system.db')
+
+    cursor = connection.cursor()
+    # 这里使用了sql拼接，可能会存在sql注入
+    sql = "select id, name, age, gender, cls from student where "+sql_special
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if result is None:
+        return None
+    student_list = []
+    for student_tuple in result:
+        id, name, age, gender, cls = student_tuple
+        student = Student(id=id, name=name, age=age, gender=gender, cls=cls)
+        print(student.getmessage())
+        student_list.append(student)
+    # 关闭连接
+    cursor.close()
+    connection.close()
+    return student_list
 
 def update_student(student):
     """
     TODO 修改学生的信息，其中student.id做为修改的查询条件
     :param student: 学生对象，其中id不能改变，其余信息为要修改的值
-    :return: None
+    :return: 受影响的数
     """
-    pass
+    if student.id is None:
+        return 0
+    connection = sqlite3.connect(r'../sqlite/student_system.db')
+    cursor = connection.cursor()
+    sql = "update student set name=? and age = ? and gender = ? and cls = ? where id = ?"
+    cursor.execute(sql, (student.name, student.age, student.gender, student.cls, student.id))
+    # 关闭连接，提交事务
+    cursor.close()
+    connection.commit()
+    connection.close()
+    # 返回数据库的改变数
+    return cursor.rowcount
 
 
 
+"""
+    测试
+"""
+# if __name__ == '__main__':
+#     from entry.Student import Student
+    # student = Student(name='雷军', cls='计本1905班', gender='男', age=25)
+    # length = add_student(student)
+    # if length == 1:
+    #     print('添加成功')
+
+    # length = delete_student(16)
+    # if length == 1:
+    #     print('删除成功')
+
+    # 测试根据id查询学生信息
+    # select_one_student(1)
+
+    # 测试返回所有学生的信息
+    # select_all_student()
+
+    # 测试特殊sql查询
+    # select_special_student("age<20")
+
+    # 测试修改学生的信息
+    # student = Student(id=1, name='王小波', age=40, gender='男', cls='物本1586')
+    # result = update_student(student)
+    # if result == 1:
+    #     print('修改成功')
+    # else:
+    #     print('修改失败')
